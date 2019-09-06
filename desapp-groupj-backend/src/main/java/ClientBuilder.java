5e3233a5a354b68ee1ab0935c61799892274dd27
TheCask <eugeniocalcena@gmail.com>
2019-09-05 21:27:43 -0300

    ClientBuilder and Client

---
+++ desapp-groupj-backend/src/main/java/ClientBuilder.java
@@ -0,0 +1,28 @@
+public class ClientBuilder {
+    public static ClientBuilder aClient() {
+        return new ClientBuilder();
+    }
+
+    private String firstName = "no firstName";
+    private String lastName = "no lastName";
+    private String mail = "no@mail.com";
+    private String phone = "0000-00000000";
+    private String city = "no city";
+    private String address = "no address";
+    private int credit = 0;
+
+    public Client build() {
+        return new Client(firstName, lastName, mail, phone, city, address, credit);
+    }
+
+    public ClientBuilder withName(final String aFirstName, final String aLastName) {
+        firstName = aFirstName;
+        lastName = aLastName;
+        return this;
+    }
+
+    public ClientBuilder withCredit(int initialCredit) {
+        credit = initialCredit;
+        return this;
+    }
+}
---
+++ desapp-groupj-backend/src/test/java/ClientTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+        import static org.junit.Assert.*;
+
+public class ClientTest {
+    @Test
+    public void chargeCreditToAccount_5000() {
+        int credit = 5000;
+        Client aClient = ClientBuilder.aClient().withCredit(0).build();
+
+        aClient.chargeCredit(credit);
+
+        assertEquals(credit, aClient.getCredit());
+    }
+}
