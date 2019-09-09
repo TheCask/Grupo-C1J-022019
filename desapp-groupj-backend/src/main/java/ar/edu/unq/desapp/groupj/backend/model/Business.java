b50a8eae2fa9a728be4c6739a8c5d6aefdfd9159
mgarzonunq <marcelo.garzon@gmail.com>
2019-09-08 23:40:34 -0300

    Adding basic Menu implementation...

---
+++ desapp-groupj-backend/src/main/java/ar/edu/unq/desapp/groupj/backend/model/Business.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.desapp.groupj.backend.model;
+
+public class Business {
+
+}
---
+++ desapp-groupj-backend/src/main/java/ar/edu/unq/desapp/groupj/backend/model/MenuCategory.java
@@ -0,0 +1,11 @@
+package ar.edu.unq.desapp.groupj.backend.model;
+
+public enum MenuCategory {
+    Pizza,
+    Cerveza,
+    Hamburguesa,
+    Sushi,
+    Empanadas,
+    Green,
+    Vegano
+}
